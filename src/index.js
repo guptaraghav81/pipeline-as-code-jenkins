af4742524890c86e01f1a4d8908696ff42bf6412
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-04-21 19:55:17 +0530

    redux setUp incomplete

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "redux-toolkit"
-// const store = configureStore({
-  // reducer:rootReducer,
-// })
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+const store = configureStore({
+  reducer:rootReducer,
+})
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
     <BrowserRouter>
        <App />
     </BrowserRouter>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,7 @@
 import {combineReducers} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
+
+export default rootReducer;
