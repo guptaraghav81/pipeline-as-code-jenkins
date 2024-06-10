a2534a3f274e154574057fc7779475e161c35730
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-06-10 23:51:55 +0530

    enroll course

--- src/Components/Core/Course/CourseDetailsCard.jsx
+++ src/Components/Core/Course/CourseDetailsCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+// import copy from "copy-to-clipboard"
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { ACCOUNT_TYPE } from '../../../utils/constants';
 import { addToCart } from '../../../Slices/cartSlice';
+import { BsFillCaretRightFill } from "react-icons/bs"
+import { FaShareSquare } from "react-icons/fa"
 import toast from 'react-hot-toast';
 
 const CourseDetailsCard = ({course, setConfirmationModal, handleBuyCourse}) => {
@@ -15,7 +18,10 @@ const CourseDetailsCard = ({course, setConfirmationModal, handleBuyCourse}) => {
         price: CurrentPrice,
 
     } = course;
-
+    // const handleShare = () => {
+    //     copy(window.location.href)
+    //     toast.success("Link copied to clipboard")
+    //   }
     const handleAddToCart = async ()=>{
         if(user && user?.accountType === ACCOUNT_TYPE.INSTRUCTOR) {
             toast.error("You are an Instructor, you cant buy a course");
@@ -43,6 +49,7 @@ const CourseDetailsCard = ({course, setConfirmationModal, handleBuyCourse}) => {
                 className='max-h-[300px] min-h-[180px] w-[400px] rounded-xl'
             />
             <div>
+            <div>
                 Rs. {CurrentPrice}
             </div>
             <div className='flex flex-col gap-y-6'>
@@ -68,6 +75,37 @@ const CourseDetailsCard = ({course, setConfirmationModal, handleBuyCourse}) => {
                     )
                 }
             </div>
+            <div>
+            <p className="pb-3 pt-6 text-center text-sm text-richblack-25">
+              30-Day Money-Back Guarantee
+            </p>
+          </div>
+
+          <div className={``}>
+            <p className={`my-2 text-xl font-semibold `}>
+              This Course Includes :
+            </p>
+            <div className="flex flex-col gap-3 text-sm text-caribbeangreen-100">
+              {course?.instructions?.map((item, i) => {
+                return (
+                  <p className={`flex gap-2`} key={i}>
+                    <BsFillCaretRightFill />
+                    <span>{item}</span>
+                  </p>
+                )
+              })}
+            </div>
+          </div>
+          <div className="text-center">
+            <button
+              className="mx-auto flex items-center gap-2 py-6 text-yellow-100 "
+            //   onClick={handleShare}
+            >
+              <FaShareSquare size={15} /> Share
+            </button>
+          </div>
+            </div>
+
     </div>
   )
 }
